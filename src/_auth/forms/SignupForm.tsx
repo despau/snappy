8f6e5d310a9434422dcf981fdaf6c25a43a65e9f
Codear <raugustin1049@gmail.com>
2023-12-27 21:40:39 -0500

    added shadcnui library and setup project styling

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+
+const SignupForm = () => {
+  return (
+    <div>
+      <h3>SignupForm</h3>
+      <Button>Click Me</Button>
+    </div>
+    
+  )
+}
+
+export default SignupForm
