a82c3718acd3ff5cffd926e39714adfc4a4bbd98
Codear <raugustin1049@gmail.com>
2024-01-07 19:54:43 -0500

    adding navigation and pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import SigninForm from './_auth/forms/SigninForm';
-import { Home } from './_root/pages';
+import { Home, Explore, Saved, AllUsers, CreatePost, EditPost, PostDetails, Profile, UpdateProfile } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import SignupForm from './_auth/forms/SignupForm';
 import RootLayout from './_root/RootLayout';
@@ -22,6 +22,14 @@ const App = () => {
           {/* private routes */}
           <Route element={ <RootLayout /> } >
             <Route index element={ <Home /> } />
+            <Route path="/explore" element={ <Explore /> } />
+            <Route path="/saved" element={ <Saved /> } />
+            <Route path="/all-users" element={ <AllUsers /> } />
+            <Route path="/create-post" element={ <CreatePost /> } />
+            <Route path="/update-post/:id" element={ <EditPost /> } />
+            <Route path="/posts/:id" element={ <PostDetails /> } />
+            <Route path="/profile/:id/*" element={ <Profile /> } />
+            <Route path="/update-profile/:id" element={ <UpdateProfile /> } />
           </Route>
 
         </Routes>
